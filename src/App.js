14abc38dd061ceed013edab92e39e85e0c04097d
Kevin <klzheng@buffalo.edu>
2022-08-23 19:20:43 -0400

    Implemented functions to create a working game
    - Added tenzie state to see whether or not the game is over
    - Implemented useEffect() so that the program would check on every dice state change to essentially see if the player has won
    - Added holdDice function
    - Added rollDice function
    - Added generateNewDice function
    - Added gameOver function
    - Added different colors on button depending on if it was toggled or not
    - Added confetti effect when player has won

--- src/App.js
+++ src/App.js
@@ -1,43 +1,102 @@
 import React from "react"
+import Die from "./Die"
+import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti'
 import "./App.css"
-import "./Die"
+
 
 function App() {
 
+
     const [dice, setDice] = React.useState(createNewDie())
+    const [tenzie, setTenzie] = React.useState(false)
+
+
+    React.useEffect(() => {
+        const firstValue = dice[0].value
+        const allHold = dice.every(item => item.isHeld === true) 
+        const sameValue = dice.every(item => item.value === firstValue)
+        if (allHold && sameValue) {
+            setTenzie(true)
+        }  
+    }, [dice])
+
 
     function createNewDie() {
         const diceArray = []
         for (let i=0; i<10; i++) {
-            const randomNum = Math.ceil(Math.random()*6)
-            diceArray.push({value: randomNum, onHold: false})
+            diceArray.push(generateNewDice())
         }
         return diceArray
     }
-    console.log(dice)
+
+
+    const diceElements = dice.map(item => {
+        return (
+            <Die 
+                key={item.id}
+                value={item.value} 
+                isHeld={item.isHeld}
+                holdDice={() => holdDice(item.id)}
+            />
+        )
+    })
+
+
+    function holdDice(id) {
+        setDice(prevState => prevState.map(item => {
+            return (
+                item.id === id ? 
+                {...item, isHeld: !item.isHeld} :
+                item
+            )
+        }))
+    }
+
+    function rollDice() {
+        if (tenzie) {
+            gameOver()
+        } else {
+            setDice(prevState => prevState.map(item => {
+                return item.isHeld ?
+                    item :
+                    generateNewDice()
+            })) 
+        }
+    } 
+
+
+    function generateNewDice() {
+        const randomNum = Math.ceil(Math.random()*6)
+        return ({
+            value: randomNum, 
+            isHeld: false, 
+            id: nanoid()
+        })
+    }
+
+
+    function gameOver() {
+        setTenzie(false)
+        setDice(createNewDie())
+    }
+
 
     return(
         <div className="main-container">
+            {tenzie && <Confetti />}
             <h1>Tenzies</h1>
             <p>
                 Roll until all dice are the same. Click each die to freeze
                 it at its current value between rolls.
             </p>
             <div className="dice-container">
-                <div className="dice">1</div>
-                <div className="dice">1</div>
-                <div className="dice">1</div>
-                <div className="dice">1</div>
-                <div className="dice">1</div>
-                <div className="dice">1</div>
-                <div className="dice">1</div>
-                <div className="dice">1</div>
-                <div className="dice">1</div>
-                <div className="dice">1</div>
+                {diceElements}
             </div>
-            <button>Roll</button>
+            <button onClick={rollDice}>{tenzie ? "Next Game" : "Roll"}</button>
         </div>
     )
 };
+
 
 export default App
--- src/Die.js
+++ src/Die.js
@@ -1,8 +1,18 @@
+import { getQueriesForElement } from "@testing-library/react"
 import React from "react"
 
-function Die() {
+function Die(props) {
+
+
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
+
+
     return (
-        <></>
+        <div style={styles} className="dice" onClick={props.holdDice}>
+            <p>{props.value}</p>
+        </div>
     )
 }
 
