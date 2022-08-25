5fc472c27555035e988fea7250b542307660eac6
Kevin <klzheng@buffalo.edu>
2022-08-24 23:09:36 -0400

    Fixed timer bug
    - Timer now starts when player clicks either "Roll" button or a die
    - Added comments

--- src/App.js
+++ src/App.js
@@ -12,12 +12,13 @@ function App() {
     const [isActive, setIsActive] = React.useState(true)
     const [isStopped, setIsStopped] = React.useState(true)
     const [seconds, setSeconds] = React.useState(0)
-
+    // Initializes highScore if necessary
     if (!localStorage.getItem("highScore")) {
         localStorage.setItem("highScore", "0")
     }
 
 
+    // Sets the win condition, only if all the dice values are the same and if all dice are held
     React.useEffect(() => {
         const firstValue = dice[0].value
         const allHold = dice.every(item => item.isHeld === true) 
@@ -28,6 +29,7 @@ function App() {
     }, [dice])
 
 
+    // Runs timer while game is not won
     React.useEffect(() => {
         let int = null
         if (tenzie === false && isActive && isStopped === false) {
@@ -41,29 +43,34 @@ function App() {
     }, [isActive, isStopped, tenzie])
 
 
+    //  When game is over, saves player score to high score if applicable
     React.useEffect(() => {
         if (tenzie) {
             stopTimer()
-            console.log(getHighScore())
-            if (getHighScore() === "0") {
-                saveNewScore()
-            }
-            else if (seconds < Number(getHighScore())) {
-                saveNewScore()
-            }
+            saveNewScore()
         } 
-    }, [tenzie, highScore, seconds])
+    }, [tenzie])
 
 
+    // Retrieves high score from localStorage
     function getHighScore() {
         return localStorage.getItem("highScore")
     }
 
+
+    // Saves new score as high score if score is lower than high score
     function saveNewScore() {
-        localStorage.setItem("highScore", JSON.stringify(seconds))
-        setHighScore(seconds)
+        if (getHighScore() === "0") {
+            localStorage.setItem("highScore", JSON.stringify(seconds))
+            setHighScore(seconds)
+        }
+        else if (seconds < Number(getHighScore())) {
+            localStorage.setItem("highScore", JSON.stringify(seconds))
+            setHighScore(seconds)
+        }
     }
 
+    // Creates new array containing die objects
     function createNewDice() {
         const diceArray = []
         for (let i = 0; i < 10; i++) {
@@ -73,6 +80,65 @@ function App() {
     }
 
 
+    // Changes die 'isHeld' value if die id matches the argument id
+    function holdDice(id) {
+        setDice(prevState => prevState.map(item => {
+            return item.id === id ? 
+                {...item, isHeld: !item.isHeld} :
+                item
+        }))
+    }
+
+
+    // Rolls dice. If game is over -> resets game 
+    function rollDice() {
+        if (tenzie) {
+            setTenzie(false)
+            setDice(createNewDice())
+            resetTimer()
+        } else {
+            startTimer()
+            setDice(prevState => prevState.map(item => {
+                return item.isHeld ?
+                    item :
+                    generateNewDice()
+            })) 
+        }
+    } 
+
+
+    // Creates a new singular die
+    function generateNewDice() {
+        const randomNum = Math.ceil(Math.random()*6)
+        return ({
+            value: randomNum, 
+            isHeld: false, 
+            id: nanoid()
+        })
+    }
+
+
+    // Starts timer
+    function startTimer() {
+        setIsActive(true)
+        setIsStopped(false)
+    }
+
+
+    // Stops timer
+    function stopTimer() {
+        setIsStopped(!isStopped)
+    }
+
+
+    // Resets timer
+    function resetTimer() {
+        setIsActive(false)
+        setSeconds(0)
+    }
+
+
+    // Generates <Die> components with props 
     const diceElements = dice.map(item => {
         return (
             <Die 
@@ -86,52 +152,7 @@ function App() {
     })
 
 
-    function holdDice(id) {
-        setDice(prevState => prevState.map(item => {
-            return item.id === id ? 
-                {...item, isHeld: !item.isHeld} :
-                item
-        }))
-    }
-
-    function rollDice() {
-        if (tenzie) {
-            setTenzie(false)
-            setDice(createNewDice())
-            resetTimer()
-        } else {
-            setDice(prevState => prevState.map(item => {
-                return item.isHeld ?
-                    item :
-                    generateNewDice()
-            })) 
-        }
-    } 
-
-
-    function generateNewDice() {
-        const randomNum = Math.ceil(Math.random()*6)
-        return ({
-            value: randomNum, 
-            isHeld: false, 
-            id: nanoid()
-        })
-    }
-
-    function startTimer() {
-        setIsActive(true)
-        setIsStopped(false)
-    }
-
-    function stopTimer() {
-        setIsStopped(!isStopped)
-    }
-
-    function resetTimer() {
-        setIsActive(false)
-        setSeconds(0)
-    }
-
+    // JSX
     return(
         <div className="main-container">
             {tenzie && <Confetti />}
